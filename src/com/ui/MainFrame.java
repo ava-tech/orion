abd78d567e76cfeaef9c818143f3bfbbed337774
Kaiser Alam <kaiseralam@msn.com>
2017-04-17 01:18:57 -0700

    First swing project

---
+++ src/com/ui/MainFrame.java
@@ -0,0 +1,35 @@
+package com.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame{
+
+    private JTextArea textArea;
+    private JButton btn;
+
+
+    public MainFrame(){
+        super("Hello World");
+        setLayout(new BorderLayout());
+
+        textArea = new JTextArea();
+        btn =  new JButton("Click Me");
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.append("Hello\n");
+            }
+        });
+
+        add(textArea, BorderLayout.CENTER);
+        add(btn, BorderLayout.SOUTH);
+
+        setSize(500, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
